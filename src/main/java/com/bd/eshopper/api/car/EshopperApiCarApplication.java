64124c589df69db7171c87378ea38ad42ebe386e
zaghir <zaghir.youssef@gmail.com>
2019-11-21 12:30:27 +0100

    first commmit

---
+++ src/main/java/com/bd/eshopper/api/car/EshopperApiCarApplication.java
@@ -0,0 +1,13 @@
+package com.bd.eshopper.api.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopperApiCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopperApiCarApplication.class, args);
+	}
+
+}
